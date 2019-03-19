af6d33ce6e85d37afe403b3de50233be94f13ddd
art <partypeopleland@gmail.com>
2019-03-19 16:54:29 +0800

    正常流程 ok

--- src/PagingHelper.js
+++ src/PagingHelper.js
@@ -1,8 +1,8 @@
 class PagingHelper {
-  GetPages() {
-    return [1, 2, 3, 4, 5, 6, 7];
+  constructor() {
+    this.range = 3;
+    this.minPage = 1;
   }
-
   IsEnableNext(page, maxPage) {
     if (page >= maxPage) return false;
     return true;
@@ -11,6 +11,49 @@ class PagingHelper {
   IsEnableLast(page, maxPage) {
     if (maxPage <= 1 || page <= 1) return false;
     return true;
+  }
+  GetPages(page, maxPage) {
+    let result = Array.from(Array(7).keys(), x => x + 1);
+    let mid = this.GetMiddleNumber(page, maxPage);
+    result[0] = this.minPage;
+    result[1] = this.GetNearNumber(this.minPage, mid - 1);
+    result[2] = mid - 1;
+    result[3] = mid;
+    result[4] = mid + 1;
+    result[5] = this.GetNearNumber(mid + 1, maxPage);
+    result[6] = maxPage;
+    return result;
+  }
+
+  /**
+   * 取得相鄰的數字，不相鄰則回傳0
+   *
+   * @param {*} last
+   * @param {*} next
+   * @returns
+   * @memberof PagingHelper
+   */
+  GetNearNumber(last, next) {
+    return last + 1 === next - 1 ? last + 1 : 0;
+  }
+  /**
+   * 取得中間的數字
+   *
+   * @param {*} page
+   * @param {*} maxPage
+   * @returns
+   * @memberof PagingHelper
+   */
+  GetMiddleNumber(page, maxPage) {
+    let mid;
+    if (page <= 4) {
+      mid = this.minPage + this.range;
+    } else if (4 <= page && page < maxPage - this.range) {
+      mid = page;
+    } else {
+      mid = maxPage - this.range;
+    }
+    return mid;
   }
 }
 
--- test/PagingHelper.test.js
+++ test/PagingHelper.test.js
@@ -7,6 +7,47 @@ let sut = null;
 beforeEach(() => {
   sut = new PagingHelper();
 });
+
+describe("GetPages()", () => {
+  let dataSet = [
+    { page: 1, maxPage: 10, expected: [1, 2, 3, 4, 5, 0, 10] },
+    { page: 2, maxPage: 10, expected: [1, 2, 3, 4, 5, 0, 10] },
+    { page: 3, maxPage: 10, expected: [1, 2, 3, 4, 5, 0, 10] },
+    { page: 4, maxPage: 10, expected: [1, 2, 3, 4, 5, 0, 10] },
+    { page: 5, maxPage: 10, expected: [1, 0, 4, 5, 6, 0, 10] },
+    { page: 6, maxPage: 10, expected: [1, 0, 5, 6, 7, 0, 10] },
+    { page: 7, maxPage: 10, expected: [1, 0, 6, 7, 8, 9, 10] },
+    { page: 10, maxPage: 10, expected: [1, 0, 6, 7, 8, 9, 10] }
+  ];
+  dataSet.forEach(d => {
+    it(`頁面 ${d.page}/${d.maxPage}，應回傳${d.expected}`, () => {
+      let actual = sut.GetPages(d.page, d.maxPage);
+      actual.should.be.deep.equal(d.expected);
+    });
+  });
+});
+
+describe("GetMiddleNumber()", () => {
+  let dataSet = [
+    { page: 1, maxPage: 10, expected: 4 },
+    { page: 2, maxPage: 10, expected: 4 },
+    { page: 3, maxPage: 10, expected: 4 },
+    { page: 4, maxPage: 10, expected: 4 },
+    { page: 5, maxPage: 10, expected: 5 },
+    { page: 6, maxPage: 10, expected: 6 },
+    { page: 7, maxPage: 10, expected: 7 },
+    { page: 8, maxPage: 10, expected: 7 },
+    { page: 9, maxPage: 10, expected: 7 },
+    { page: 10, maxPage: 10, expected: 7 }
+  ];
+  dataSet.forEach(d => {
+    it(`頁面 ${d.page}/${d.maxPage} 中位數應為${d.expected}`, () => {
+      let actual = sut.GetMiddleNumber(d.page, d.maxPage);
+      actual.should.be.equal(d.expected);
+    });
+  });
+});
+
 let dataSet = [
   { page: 1, maxPage: 10, expected: [false, true] },
   { page: 2, maxPage: 10, expected: [true, true] },
@@ -19,6 +60,7 @@ let dataSet = [
   { page: 9, maxPage: 10, expected: [true, true] },
   { page: 10, maxPage: 10, expected: [true, false] }
 ];
+
 describe("IsEnableLast()", () => {
   dataSet.forEach(d => {
     it(`頁面：${d.page}/${d.maxPage} 時，上一頁按鈕顯示？ ${
